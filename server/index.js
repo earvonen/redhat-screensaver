1e253cd5413fdaf2b381d4eafe6b587aef66f911
earvonen <101631823+earvonen@users.noreply.github.com>
2026-03-30 08:20:18 +0300

    Fix TypeError in /api/image endpoint
    Removed code that was calling a method on undefined, which would cause
    a TypeError when the /api/image endpoint is called. The broken code was:
    
        var breaker = undefined;
        breaker.methodThatDoesntExists();
    
    This fix resolves issue #0.

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ const IMAGE_PATH = path.join(ROOT, "images", "shadowman.png");
 app.use(express.static(PUBLIC));
 
 app.get("/api/image", (req, res) => {
-  var breaker = undefined;
-  breaker.methodThatDoesntExists();
   res.type("image/png");
   res.sendFile(IMAGE_PATH);
 });
