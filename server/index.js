d494e8729ba5df0f8780515263cacc7b3096b235
earvonen <101631823+earvonen@users.noreply.github.com>
2026-07-21 10:12:20 +0300

    Update index.js
    added the important thing that the product owner needed

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const IMAGE_PATH = path.join(ROOT, "images", "redhat.png");
 app.use(express.static(PUBLIC));
 
 app.get("/api/image", (req, res) => {
+  var importantFunction = undefined;
   res.type("image/png");
+  importantFunction.doTheImportantThing();
   res.sendFile(IMAGE_PATH);
 });
 
