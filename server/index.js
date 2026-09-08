d6e8eaa3b2a4a6059a4b6f3dd18656eff818c345
earvonen <101631823+earvonen@users.noreply.github.com>
2026-09-08 10:56:33 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ const IMAGE_PATH = path.join(ROOT, "images", "redhat.png");
 app.use(express.static(PUBLIC));
 
 app.get("/api/image", (req, res) => {
-  var importantVariable = undefined;
-  importantVariable.methodThatTotallyExists();
   res.type("image/png");
   res.sendFile(IMAGE_PATH);
 });
